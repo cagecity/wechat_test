8e7f834e5c5b33758aa779755bb2d3a0050419d3
cagecity <oltra.cn@gmail.com>
2022-01-20 10:25:50 +0800

    change

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,12 @@
 Page({
   data:{
-    buttons: Array.from({length:25},(item, index) => index+1),
+    buttons: Array.from({length:25},(v, i) => i+1),
     taped : ""
   },
   matrixTap:function(event) {
     this.setData({
       taped : "buttonTaped"
     })
-    console.log(event.currentTarget)
+    console.log({length:5})
   }
 })
